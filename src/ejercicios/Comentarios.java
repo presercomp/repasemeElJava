29bb13bbf10706ac6abbc0b6e274af6fd838803d
PRESERCOMP <desarrollo@presercomp.cl>
2020-07-29 10:58:48 -0400

    Carga de ejercicios

---
+++ src/ejercicios/Comentarios.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+public class Comentarios {
+    // Comentario MonoLinea (una sola línea)
+    
+    /* 
+    Comentario Multi Linea
+    lo que me permite saltar y seguir escribiendo
+    sin afectar al codigo.
+    */
+
+    /**
+     * Comentario de Documentación
+     */
+    
+}
---
+++ src/ejercicios/VariablesYConstantes.java
@@ -0,0 +1,33 @@
+package ejercicios;
+
+public class VariablesYConstantes {
+    /* 
+    Una variable es un espacio de memoria 
+    que me permite almacenar un dato de un tipo de dato
+    específico. 
+    El dato puede cambiar, pero el tipo de datos no.
+    Las variables deben ser declaradas en minúscula.
+    */
+    //Tipos datos: Clasificación
+    // Datos primitivos
+    /*
+    Numericos entero: 
+    ==================
+    byte     -128 al 127
+    short    -32.768 al 32.767
+    int      -2.147.483.648 al 2.147.483.647
+    long     -2^63 al 2^63
+    ==================
+    
+    Numéricos decimales
+    ===================
+    float   -32.768^63 al 32.768^63
+    double  -64.768^63 al 64.768^63
+    ===================
+    
+    Lógicos (boolean)
+    Caracteres (char)
+    
+    */
+    // Datos no privitivos o Clases
+}
